303c885a8e9f90603aa60cc5c76afeee00d0e109
Zach Harwood <320zach@gmail.com>
2025-01-27 23:27:45 -0500

    enhanced carousel functionality with improved captions and animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,25 @@ module.exports = {
   theme: {
     extend: {
       keyframes:{
+        slideIn:{
+          from:{
+            opacity:"0",
+            transform:"translateY(-10px)",
+          },
+          to:{
+            opacity:"1",
+            transform:"translateY(0)",
+          }
+        },
+
         fades:{
           '0%':{opacity:.4},
           '100%':{opacity:1}
         }
       },
       animation:{
-        fades:'fades 1s ease-in-out '
+        fades:'fades 1s ease-in-out',
+        slideIn:'slideIn 1.5s ease-in-out'
       }
     },
   },
