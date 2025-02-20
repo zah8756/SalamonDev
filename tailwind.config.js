92de709c33fb7d800ab9cbd71766014a22a4930b
Zach Harwood <320zach@gmail.com>
2025-02-20 00:53:46 -0500

    update fonts and improve text styling for better readability and aesthetics

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,46 +1,47 @@
-const { transform } = require('sucrase');
+const { transform } = require("sucrase");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
-      fontFamily:{
-        serif: ['tinos-regular']
+      fontFamily: {
+        head: ["Bodoni Moda", "serif"],
+        body: ["Montserrat", "serif"],
       },
 
-      colors:{
-        'background-light':'#f2f8fd',
-        'hover':'#C4DFF0',
-        'text-dark':'#1E1F24',
-        'text-light':'#c3e0f4',
-        'borderG':'#B3B5C1',
-        'footerC':'#73bbe7',
-        'ButtonC':'#73BCE9'
+      colors: {
+        "background-light": "#f2f8fd",
+        hover: "#C4DFF0",
+        "text-dark": "#1E1F24",
+        "text-light": "#c3e0f4",
+        borderG: "#B3B5C1",
+        footerC: "#73bbe7",
+        ButtonC: "#73BCE9",
       },
 
-      keyframes:{
-        slideIn:{
-          from:{
-            opacity:"0",
-            transform:"translateY(-10px)",
+      keyframes: {
+        slideIn: {
+          from: {
+            opacity: "0",
+            transform: "translateY(-10px)",
           },
-          to:{
-            opacity:"1",
-            transform:"translateY(0)",
-          }
+          to: {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
         },
 
-        fades:{
-          '0%':{opacity:.4},
-          '100%':{opacity:1}
-        }
+        fades: {
+          "0%": { opacity: 0.4 },
+          "100%": { opacity: 1 },
+        },
       },
-      animation:{
-        fades:'fades 1s ease-in-out',
-        slideIn:'slideIn 1.5s ease-in-out'
-      }
+      animation: {
+        fades: "fades 1s ease-in-out",
+        slideIn: "slideIn 1.5s ease-in-out",
+      },
     },
   },
   plugins: [],
-}
+};
